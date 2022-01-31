2e2d74111de2fa962e9f6ed211e3f65b32ea0a62
Young Zoo Park <enkeejuniour@naver.com>
2022-01-31 11:00:26 +0900

    크롤링

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 
 * chrome driver version : 97 기준
 * OS : Window 64bit
+
+* 사용자 인풋
+  ID, PW 변수에 bybit 사이트의 아이디, 비밀번호 입력
--- auto_scraping.py
+++ auto_scraping.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
 '''
-1. 로그인
-2. BTCUSDT
-3. INDICATOR 실시간으로 받아오기
-4. 시각화/DataFrame으로 저장도 해주면 좋을 듯함
-
-* 각 단계에서 assert 진행하기
-
 '''
 
 import json
@@ -61,57 +54,30 @@ def crawler_D1():
     element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__layout"]/div/main/div/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div/div[1]/div[2]/input')))
 
     ID = driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div/div/div/div[1]/div[2]/div/div[1]/div[1]/div/div[1]/div[2]/input')
-    ID.send_keys('enkeejuniour@naver.com') # 아이디 입력하기
+    # 아이디 입력 #
+    ID.send_keys('') # 아이디 입력하기
     PW = driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div/div/div/div[1]/div[2]/div/div[1]/div[2]/div[1]/div[2]/input')
-    PW.send_keys('Qkrdydwn1*') # 비밀번호 입력하기
-    driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div/div/div/div[1]/div[2]/div/div[2]/div').send_keys(Keys.ENTER)
+    # 패스워드 입력 #
+    PW.send_keys('') # 비밀번호 입력하기
+    element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="__layout"]/div/main/div/div/div/div/div[1]/div[2]/div/div[2]/div/a')))
 
-    # 게시판 접근
-    log_page = 'https://www.bybit.com/trade/usdt/BTCUSDT'
-    driver.get(log_page)
+    driver.find_element_by_xpath('//*[@id="__layout"]/div/main/div/div/div/div/div[1]/div[2]/div/div[2]/div/a').click()
+
+
     
     # 이미지를 허용해두어야 함
-    # 굴뚝 허용하기 문제를 해결해야 함 - 이때는 그냥 다시 시도하는 편이 나음. 
-    Table_box_ = driver.find_element_by_css_selector(
-            '#posts-filter > table')
-    Table_box = Table_box_.find_element_by_id('the-list')
+    try:
+        # 게시판 접근
+        log_page = 'https://www.bybit.com/trade/usdt/BTCUSDT' #https://www.bybit.com/trade/usdt/BTCUSDT
+        driver.get(log_page)
+        
+        # 값 찾기
+        price = driver.find_element_by_xpath('//*[@id="root"]/main/div[1]/aside/div[1]/div[1]/div/span')
 
-    try:  # 맨 마지막 페이지 핸들링용
-        for idx in range(1, 21):  # 테이블 긁어오기
-            href = Table_box.find_element_by_xpath(
-                f'/html/body/div[1]/div[2]/div[2]/div[1]/div[3]/form[1]/table/tbody/tr[{idx}]/td[1]/strong/a')
-            link = href.get_attribute('href')
-            actor = Table_box.find_element_by_xpath(
-                f'/html/body/div[1]/div[2]/div[2]/div[1]/div[3]/form[1]/table/tbody/tr[{idx}]/td[2]/a').text
-
-            date = Table_box.find_element_by_xpath(
-                f'/html/body/div[1]/div[2]/div[2]/div[1]/div[3]/form[1]/table/tbody/tr[{idx}]/td[3]').text
-            # 맨 앞은 2022/, 쉼표 뒤는 시간이므로
-            date_cleaned = date[9: date.find(',')]
-            date_cleaned = re.sub('/', '', date_cleaned)
-
-            # print(date_cleaned)
-
-            if (date_cleaned in DATE_range) and (actor in SEED_ls):  # 스누씨드가 아닌 경우에 굳이 긁을 필요 없음
-                Result['href_ls'].append(link)
-                Result['Actor_name'].append(actor)
-                Result['Date'].append(date)
-    except NoSuchElementException:
-            print(f'Crawling End in page..{page_num}')
-            break  # 가장 마지막 페이지일 것이므로
-        # Next_page = driver.find_element_by_class_name('pagination_links')
-        # Next_page.click()
-    Next_page = log_page + r'&' + f'paged={page_num}'
-    driver.get(Next_page)
-
-    # 엑셀에 저장한다. 
-    save_folder = 'SNURO_Homepage'
-    Full_path = os.path.join(os.getcwd(), save_folder, result_name)
-    df = pd.DataFrame.from_dict(Result)
-    assert df.shape[0] > 1
-    df.to_excel(Full_path)
-
-    return Result  # dictionary"""
+        while True:
+            print(price.text, time.strftime('%Y/%m/%d %H:%M:%S', time.localtime()))
+    except:
+        print('보안 강화 : ex - 그림 찾기 등')
 
 
 def crawler_D2(dframe,  # 매칭정보
@@ -153,12 +119,6 @@ if __name__ == '__main__':
     driver = webdriver.Chrome(executable_path=selen_path,
                               chrome_options=chrome_opt)
     RS1 = crawler_D1()
-    print('>>>>> Crawling RS: ', len(RS1['href_ls']))
-
-    RS2 = crawler_D2(dframe=df,
-                     output1=pd.read_excel(os.path.join(
-                         os.getcwd(), 'SNURO_Homepage', '스누로_크롤링 D1.xlsx')),
-                     result_name='220130_week3_xAPI.pkl')
-    print('>>>>> Crawling RS2: ', len(RS2['href_ls']))
-
+    
+    
     # print(date_list)
